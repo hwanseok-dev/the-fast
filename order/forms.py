d3858a8b7b6ad34b09a434d4f53793a10d4afb27
hwanseok.dev <hwanseok.dev@gmail.com>
2021-02-06 22:59:35 +0900

    delete product stock according to order quantity

--- order/forms.py
+++ order/forms.py
@@ -2,11 +2,14 @@ from django import forms
 from .models import Order
 from product.models import Product
 from fcuser.models import Fcuser
+from django.db import transaction
 
 '''
 views.py의 OrderRegister Class에 의해서 인스턴스가 생성된다.
 OrderRegister Class에서 get_form_kwargs를 통해서 request kw를 추가했기 때문에 __init__에서 request를 전달받을 수 있다.
 '''
+
+
 class RegisterForm(forms.Form):
     def __init__(self, request, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -36,16 +39,20 @@ class RegisterForm(forms.Form):
         fcuser = self.request.session.get('user')
 
         if quantity and product and fcuser:
-            order = Order(
-                quantity=quantity,
-                product=Product.objects.get(pk=product),
-                fcuser=Fcuser.objects.get(email=fcuser)
-            )
-            order.save()
-        else :
+            with transaction.atomic():
+                prod = Product.objects.get(pk=product)
+                order = Order(
+                    quantity=quantity,
+                    product=prod,
+                    fcuser=Fcuser.objects.get(email=fcuser)
+                )
+                order.save()
+                prod.stock -= quantity
+                prod.save()
+        else:
             # OrderRegister Class에서 template_name은 설정하지 않았습니다.
             # 이 때문에 add_error된 값을 어디에서 보여주어야하는지 모르겠다는 에러가 발생합니다.
             # 이를 막기 위해서 OrderRegister에서 form_invalid를 통해 redirect를 해줍니다.
             self.product = product
-            self.add_error('quantity','수량이 없습니다.')
-            self.add_error('product','상품이 없습니다.')
+            self.add_error('quantity', '수량이 없습니다.')
+            self.add_error('product', '상품이 없습니다.')
